a98f68c0f0f4d0c495d49f8c282fddb81aa3b3d0
aiden01 <liangu@protonmail.com>
2019-07-28 11:45:36 -0400

    feat: Repl

---
+++ src/logger.rs
@@ -0,0 +1,43 @@
+use colored::*;
+use std::io;
+use std::io::Write;
+use std::fmt::Display;
+
+enum Importance {
+    Info,
+    Success,
+    Error
+}
+
+fn format_msg(importance: Importance, msg: &str) -> ColoredString {
+    match importance {
+        Importance::Info => msg.blue(),
+        Importance::Success => msg.green(),
+        Importance::Error => msg.red()
+    }
+}
+
+pub fn error<T: AsRef<str>>(msg: T) {
+    println!("{}", format_msg(Importance::Error, msg.as_ref()));
+}
+
+pub fn success<T: AsRef<str>>(msg: T) {
+    println!("{}", format_msg(Importance::Success, msg.as_ref()));
+}
+
+pub fn info<T: AsRef<str>>(msg: T) {
+    println!("{}", format_msg(Importance::Info, msg.as_ref()));
+}
+
+pub fn prompt<T: AsRef<str> + Display>(msg: T) -> String {
+    let mut input = String::new();
+    let log = format_msg(Importance::Info, &format!("{} ➤ ", msg));
+    print!("{}", log);
+    io::stdout().flush();
+    io::stdin().read_line(&mut input).expect("Unable to read line");
+    input
+}
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate combine;
+
+use crate::combine::Parser;
+mod parser;
+mod logger;
+use logger::prompt;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let input = prompt("Enter brainfuck code");
+        let result = parser::instrs().parse(input.as_str());
+        dbg!(result);
+    }
 }
